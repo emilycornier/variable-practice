a685030454a1c296f48d6a825f51c937ac90e105
Emily Cornier <emilycornier@gmail.com>
2022-02-24 20:40:32 -0700

    Add changes to file

---
+++ variables.js
@@ -0,0 +1,15 @@
+var name = "Emily";
+var favArtist = 'Kendrick Lamar';
+
+console.log(name, favArtist);
+
+var age = 28;
+var birthYear = 1993;
+
+console.log(age, birthYear);
+
+//varible defined at isHungry is true
+var isHungry = true;
+var isSleepy = true;
+
+console.log(isHungry,isSleepy);
